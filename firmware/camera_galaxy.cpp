88050442d78bd18a00639dc43d81fdb89775260e
zhangguanjin <542493073@qq.com>
2023-06-02 17:59:05 +0800

    亮度图hdr适配大恒相机

--- firmware/camera_galaxy.cpp
+++ firmware/camera_galaxy.cpp
@@ -8,6 +8,21 @@ CameraGalaxy::~CameraGalaxy()
 {
 }
 
+
+bool CameraGalaxy::trigger_software()
+{
+    GX_STATUS status = GX_STATUS_SUCCESS;
+    // 发送软触发命令
+	status = GXSendCommand(hDevice_,GX_COMMAND_TRIGGER_SOFTWARE);
+	if(status != GX_STATUS_SUCCESS)
+	{
+        LOG(ERROR) << "GX_COMMAND_TRIGGER_SOFTWARE ERROR!";
+		return false;
+	} 
+
+    return true;
+}
+
 bool CameraGalaxy::grap(unsigned char *buf)
 {
     std::lock_guard<std::mutex> my_guard(operate_mutex_);
@@ -206,30 +221,33 @@ bool CameraGalaxy::switchToInternalTriggerMode()
  
 
     GX_STATUS status;
-    // status = GXSetEnum(hDevice_, GX_ENUM_LINE_SOURCE, GX_ENUM_LINE_SOURCE_STROBE); 
-    // if (GX_STATUS_SUCCESS != status)
-    // {
-    //     LOG(INFO) << "GX_ENUM_LINE_SOURCE Error: " << status;
-    //     return false;
-    // }
 
-    status = GXSetEnum(hDevice_, GX_ENUM_TRIGGER_MODE, GX_TRIGGER_MODE_OFF);  
+ 
+    status = GXSetEnum(hDevice_, GX_ENUM_TRIGGER_MODE, GX_TRIGGER_MODE_ON);  
     if (GX_STATUS_SUCCESS != status)
     {
         LOG(INFO) << "GX_ENUM_TRIGGER_MODE Error: " << status;
         return false;
     }
 
+    status =  GXSetEnum(hDevice_,GX_ENUM_TRIGGER_SOURCE, GX_TRIGGER_SOURCE_SOFTWARE);
+    if (GX_STATUS_SUCCESS != status)
+    {
+        LOG(INFO) << "GX_TRIGGER_SOURCE_SOFTWARE Error: " << status;
+        return false;
+    }
+
+ 
+    // 曝光完成事件使能
+	status = GXSetEnum(hDevice_, GX_ENUM_EVENT_NOTIFICATION, GX_ENUM_EVENT_NOTIFICATION_ON);
+    if (GX_STATUS_SUCCESS != status)
+    {
+        LOG(INFO) << "GX_ENUM_EVENT_NOTIFICATION_ON Error: " << status;
+        return false;
+    }
+
     trigger_on_flag_ = false;
-
-    // status = GXSendCommand(hDevice_, GX_COMMAND_TRIGGER_SOFTWARE);
-    // if (GX_STATUS_SUCCESS != status)
-    // {
-
-    //     LOG(INFO) << "GX_COMMAND_TRIGGER_SOFTWARE Error: " << status;
-    //     return false;
-    // }
-
+ 
     return true;
 }
 bool CameraGalaxy::switchToExternalTriggerMode()
